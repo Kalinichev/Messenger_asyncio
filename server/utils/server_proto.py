35114783cfbace206bfdf9afbde18411385af659
Kalinichev <kalinichev.peter@yandex.ru>
2025-07-13 17:23:14 +0300

    data_received

--- server/utils/server_proto.py
+++ server/utils/server_proto.py
@@ -42,3 +42,36 @@ class ChatServerProtocol(Protocol, ConvertMixin, DbInterfaceMixin):
             self.add_client_history(username)
             return True
         return False
+
+    def data_received(self, data):
+        _data = self._bytes_to_dict(data)
+        if _data:
+            try:
+                if _data['action'] == 'presence':
+                    if _data['user']['account_name']:
+                        resp_msg = self.jim.response(code=200)
+                        self.transport.write(self._dict_to_bytes(resp_msg))
+                    else:
+                        resp_msg = self.jim.response(code=500, error='wrong presence msg')
+                        self.transport.write(self._dict_to_bytes(resp_msg))
+                elif _data['action'] == 'authenticate':
+                    if self.authenticate(_data['user']['account_name'], _data['user']['password']):
+                        if _data['user']['account_name'] not in self.users:
+                            self.user = _data['user']['account_name']
+                            self.connections[self.transport]['username'] = self.user
+                            self.users[_data['user']['account_name']] = self.connections[self.transport]
+                            self.set_user_online(_data['user']['account_name'])
+                        resp_msg = self.jim.probe(self.user)
+                        self.users[_data['user']['account_name']]['transport'].write(self._dict_to_bytes(resp_msg))
+                    else:
+                        resp_msg = self.jim.response(code=402, error='wrong login/password')
+                        self.transport.write(self._dict_to_bytes(resp_msg))
+                # else:
+                #     resp_msg = self.jim.response(code=500, error='Неверный формат действия')
+                #     self.transport.write(self._dict_to_bytes(resp_msg))
+            except Exception as e:
+                resp_msg = self.jim.response(code=500, error=e)
+                self.transport.write(self._dict_to_bytes(resp_msg))
+        else:
+            resp_msg = self.jim.response(code=500, error='Неверный формат сообщения')
+            self.transport.write(self._dict_to_bytes(resp_msg))
